cf6776d7561b787aa74cd824552c049e34006e47
Kevin Eduardo <kevin@invoiceti.com.br>
2020-10-06 08:37:41 -0300

    Preenchidos completamente os models.

--- src/main/java/br/senai/bitatom/models/Medicamento.java
+++ src/main/java/br/senai/bitatom/models/Medicamento.java
@@ -1,8 +1,19 @@
 package br.senai.bitatom.models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data 
 public class Medicamento {
 
+	public Long id;
+	public String nome;
+	public LocalDate inicioMed;
+	public LocalDate fimMed;
+	public LocalDateTime intervaloTempo;
+	public int dose;
+	
+	
 }
--- src/main/java/br/senai/bitatom/models/Terapia.java
+++ src/main/java/br/senai/bitatom/models/Terapia.java
@@ -1,8 +1,16 @@
 package br.senai.bitatom.models;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data 
 public class Terapia {
+	
+	public LocalDate inicioTerapia;
+	public LocalDate fimTerapia;
+	public Long idMedicamentos;
+	public Long idPaciente;
+	
 
 }
